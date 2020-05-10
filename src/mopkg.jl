4cb290897e56b0fb09f68151cd718a7e544904fb
daniop <daniop12345@gmail.com>
2020-05-10 10:57:44 +0200

    add function and tests

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -1,5 +1,17 @@
 module mopkg
 
-greet() = print("Hello World!")
+"function minimize function with one variable using Newton_Raphson metod as arguments take function , derivative of this function and start point"
+function Newton_Raphson(f, df, x0; ϵ = 1e-9 )
+    new, old = x0, Inf
+    i = 0
+    while abs((new - old)/new) >= ϵ
+        x = new - (f(new)/df(new))
+        i = i + 1
+        new, old = x, new
+    end
+    return new
+end
+
+export Newton_Raphson
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using mopkg
 using Test
 
 @testset "mopkg.jl" begin
-    # Write your own tests here.
+    @test Newton_Raphson(x -> x^3 - 5x + 1, x -> 3x^2 - 5, 0) == 0.20163967572340463
+    @test Newton_Raphson(x -> exp(x) - x^4, x -> exp(x) - 4x^3, 2) == 1.4296118247255556
 end
